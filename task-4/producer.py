b6107e3ffeb6c9eade75460b2706700a67459f52
unknown <hzfang80@gmail.com>
2020-08-26 17:24:58 -0400

    first commit

---
+++ task-4/producer.py
@@ -0,0 +1,14 @@
+#kafka-python package pre-installed
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+#Kafa server running at localhost:9093
+producer = KafkaProducer(bootstrap_servers=['localhost:9093'],
+                     	value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+#send messages into the topic 'bdworld'
+with open('./Shakespeare.txt', 'r') as f:
+	for line in f.readlines():
+    	producer.send('bdworld', value=line)
+    	sleep(2)
