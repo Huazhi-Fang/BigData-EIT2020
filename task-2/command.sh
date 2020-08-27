a6d71cf714396868349fa8a23c66fcebdec6a4bd
unknown <hzfang80@gmail.com>
2020-08-27 12:49:00 -0400

    sqoop_mysql-hdfs

--- task-2/command.sh
+++ task-2/command.sh
@@ -1,10 +1,8 @@
 #Option 1:
 
 sqoop import --connect jdbc:mysql://127.0.0.1:3306/bigdata2020 --table emp --username test1 --password Abcd1234! --target-dir /sqoop_out_emp  -m 1
-
 sqoop import --connect jdbc:mysql://127.0.0.1:3306/bigdata2020 --table department --username test1 --password Abcd1234! --target-dir /sqoop_out_department  -m 1
 
 
 #Option 2:
-
 sqoop import-all-tables --connect jdbc:mysql://127.0.0.1:3306/bigdata2020 --username test1 --password Abcd1234! --warehouse-dir /sqoop  -m 1
